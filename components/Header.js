c8ad7161a3eae39c4d962d975f67df7e2e288e50
won-a1 <f.anya.00008@gmail.com>
2025-09-10 17:00:54 +0530

    .

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import logo from './logo.png';
+
+export default function Header() {
+    return (
+        <header>
+            <img src={logo} alt="Logo"/>
+            <h1>Chef AI</h1>
+        </header>
+    )
+}
---
+++ components/Main.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { getRecipeFromMistral } from "../ai"
+import ClaudeRecipe from "./ClaudeRecipe"
+import IngredientsList from "./IngredientsList"
+
+export default function Main() {
+    const [ingredients, setIngredients] = React.useState(
+        ["chicken", "all the main spices", "corn", "heavy cream", "pasta"]
+    )
+    const [recipe, setRecipe] = React.useState("")
+    const recipeSection = React.useRef(null)
+    console.log(recipeSection)
+    
+
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromMistral(ingredients)
+        setRecipe(recipeMarkdown)
+    }
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+    
+    return (
+        <main>
+            <form action={addIngredient} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+
+            {ingredients.length > 0 &&
+                <IngredientsList
+                    ref={recipeSection}
+                    ingredients={ingredients}
+                    getRecipe={getRecipe}
+                />
+            }
+
+            {recipe && <ClaudeRecipe recipe={recipe} />}
+        </main>
+    )
+}
